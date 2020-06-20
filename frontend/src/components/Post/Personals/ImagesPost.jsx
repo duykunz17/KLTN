656a304c624da813c3c576486ece32e5a5ef2efe
thanhduyPTA <zzzzthanhduyzzzz@gmail.com>
2020-06-20 19:24:22 +0700

    edit gui blog

--- frontend/src/components/Post/Personals/ImagesPost.jsx
+++ frontend/src/components/Post/Personals/ImagesPost.jsx
@@ -6,7 +6,7 @@ class ImagesPost extends Component {
     displayImagesPost = (images) => {
         return images.map((img, index) => {
             return (
-                <img key={index} src={img} className="col-md-4" style={{ height: '70px', marginBottom: '25px' }} alt="err" />
+                <img key={index} src={img} className="col-md-4" style={{ height: '70px', margin: '20px -30px -10px 10px' }} alt="err" />
             )
         })
     }
@@ -17,7 +17,7 @@ class ImagesPost extends Component {
             <div className="row">
                 <div className="col-md-12">
                     <div className="mdb-lightbox">
-                        <figure>
+                        <figure style={{marginLeft: '10px', marginBottom: '40px'}}>
                             {posts.map((item, index) => {
                                 return (
                                     <Link key={index} to={'post-detail/' + item._id} >
